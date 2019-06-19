67e12b04e1541adf43b948b4320444614ee1a55b
jdelacruz07 <jdelacruz0077@gmail.com>
2019-06-19 10:22:13 +0200

    Primer Commit

---
+++ src/Usuari.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Usuari {
+
+	String name = null;
+	String surname = null;
+	String password = null;
+	Date date = new Date();
+	
+}
---
+++ src/Video.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Video {
+
+	String url = null;
+	String title = null;
+	List<String> tags = new ArrayList<>();
+	
+	
+}
