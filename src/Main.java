84662aa9fc540994f272f03cb055c0b18fef3be1
jdelacruz07 <jdelacruz0077@gmail.com>
2019-06-20 13:06:00 +0200

    Menu creado donde falta de dar de alta al usuario

--- src/Main.java
+++ src/Main.java
@@ -6,21 +6,57 @@ import java.util.Scanner;
 public class Main {
 	static Scanner sc = new Scanner(System.in);
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		System.out.println("Dame el nombre del usuario");
-		String name = sc.next();
-		System.out.println("Dame el apellido");
-		String surname = sc.next();
-		System.out.println("Dame el password");
-		String password = sc.next();
-		System.out.println("Dame la fecha");
-		Date date = new SimpleDateFormat("dd/MM/yyyy").parse(sc.next());
+		Usuari usuari = new Usuari();
+//		Video video = new Video();
+		int select = 0;
+		do { 
+		System.out.println("1) Eres cliente ");
+		System.out.println("2) No eres cliente, hazte cliente! ");
+		System.out.println("3) Salir ");
 		
-		Usuari usuari = new Usuari(name, surname, password, date);
+		select = sc.nextInt();
+		} while  (select != 1 && select != 2 && select !=3);
 		
+		if (select == 1) {
+			System.out.print("Dame el nombre del usuario");
+			String name = sc.next();
+			usuari.verifyClient(name);
+			optionUser();
+		} else if (select == 2){
+			System.out.println("Darse de alta ");}
+		else {System.out.println("Adeuuuuu");
+		}
+		  
+	}
 		
-		
+		private static void optionUser() {
+		// TODO Auto-generated method stub
+		System.out.println("1) Crear un nuevo video");
+		System.out.println("2) Ver la lista de videos ");
 	}
 
+		public void watchVideo(){
+		System.out.print("Dame el nombre del usuario");
+		String name = sc.next();
+		name = null;
+//		name = null;
+//		System.out.println("Dame el apellido");
+//		String surname = sc.next();
+//		System.out.println("Dame el password");
+//		String password = sc.next();
+//		System.out.println("Dame la fecha");
+//		Date date = new SimpleDateFormat("dd/MM/yyyy").parse(sc.next());
+		
+		try {
+			Usuari usuari = new Usuari(name);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Video video = new Video();
+		
+		
+		}
 }
--- src/Usuari.java
+++ src/Usuari.java
@@ -6,7 +6,7 @@ public class Usuari {
 	private String surname = null;
 	private String password = null;
 	private Date date = new Date();
-	
+
 	public Usuari(String name, String surname, String password, Date date) {
 		this.name = name;
 		this.surname = surname;
@@ -15,6 +15,36 @@ public class Usuari {
 	}
 
 	public Usuari() {
+	
+	}
+	
+	public Usuari(String name) throws Exception {
 		// TODO Auto-generated constructor stub
+		System.out.println("Se crea un usuario ");
+		checkdata(name);
+
+	}
+
+	public void checkdata(String data) throws Exception {
+		if (data != null) {
+			System.out.println("Campo no vacio ");
+		} else {
+			
+				throw new Exception("Campo vacio ");
+			} 
+		
+	}
+	public boolean verifyClient(String name2) throws Exception {
+		boolean isUsuari = false;
+		this.name = name2;
+//		this.name = null;
+		if (this.name == name2) {
+			System.out.println("Cliente firmado correctamente");
+			isUsuari = true;
+		} else {
+			System.out.println("Cliente no encontrado ");
+			isUsuari = false;
+		} 
+		return isUsuari;
 	}
 }
